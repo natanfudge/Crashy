b9d4390112de68d3b6279ad02950addde7093e6b
natanfudge <natan.lifsiz@gmail.com>
2022-09-30 22:35:29 +0300

    good progress, next time we will do java based ssh

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,10 +1,27 @@
-import java.lang.ProcessBuilder.Redirect
-import java.lang.Thread
+import com.sshtools.client.SshClient
+import com.sshtools.client.scp.ScpClient
+import java.util.*
 import kotlin.math.absoluteValue
 import kotlin.random.Random
-import java.util.Scanner
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.InputStream
+import java.io.ByteArrayOutputStream
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+buildscript {
+    repositories {
+        maven (
+            url = ("https://oss.sonatype.org/content/repositories/snapshots")
+        )
+    }
 
-
+    dependencies {
+        classpath ("com.sshtools:maverick-synergy-client:3.0.6")
+    }
+}
 plugins {
     application
     alias(libs.plugins.kotlin)
@@ -19,7 +36,7 @@ group = "io.github.crashy"
 version = "0.0.2"
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -29,6 +46,11 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 repositories {
     mavenCentral()
+        maven (
+            url = ("https://oss.sonatype.org/content/repositories/snapshots")
+        )
+
+
 }
 
 val invoker = configurations.create("invoker")
@@ -36,6 +58,7 @@ val invoker = configurations.create("invoker")
 dependencies {
     implementation (libs.bundles.main)
     implementation(libs.bundles.test)
+    implementation("com.sshtools:maverick-synergy-client:3.0.6")
 }
 
 
@@ -102,6 +125,22 @@ tasks {
             commandLine("java", "-jar",serverJar.name)
         }
 
+        /**
+         * Name: Upload to EC2
+         *
+         * Operation: Replaces the server jar in the EC2 instance with the output of shadowJar.
+         *  Specifically, it copies the shadowJar into the EC2 instance using SCP, it then SSHs into the EC2, stops the running java process,
+         *  deletes the old jar, and starts the new shadowJar.
+         *
+         * Requirements:
+         * - EC2 instance:
+         *  - Configured to accept SSH
+         *  - Has java installed
+         *  - Has directory ~/jars
+         * - Project property **ec2_domain** set to the domain name of the EC2 instance.
+         * - Environment variable **EC2_KEYPAIR** set to the fully qualified path to a keypair file that can access the EC2.
+         * - Using Shadow.
+         */
         val uploadToEc2 = register("uploadToEc2") {
             group = "ec2"
             dependsOn(shadowJar)
@@ -117,7 +156,7 @@ tasks {
 
             val keyPair = System.getenv("EC2_KEYPAIR")
             val serverJarPath = serverJar.absolutePath
-            val domain = "ec2-18-195-90-15.eu-central-1.compute.amazonaws.com"
+            val domain = project.property("ec2_domain")?.toString()
             val sshTarget = "ec2-user@$domain"
             val serverDirPath = serverDir.toFile().absolutePath
             val jarName = serverJar.name
@@ -126,14 +165,14 @@ tasks {
             val sshCommandPrefix = "ssh -i $keyPair $sshTarget"
             val killCommand = "sudo killall java"
 
-            val acDir = "ac"
-            val fullAcDir = "~/$acDir"
+            val allJarsDir = "jars"
+            val fullJarsDir = "~/$allJarsDir"
 
-            val scpCommand = "scp -r -i $keyPair $serverDirPath $sshTarget:$fullAcDir/"
-            val relativeAcDir = "./$acDir"
-            val removeCommand = "sudo find $relativeAcDir -type f -not -path \"$relativeAcDir/$randomId/*\" -delete"
-            val cleanDirsCommand = "sudo find $fullAcDir -empty -type d -delete"
-            val jarDir = "$fullAcDir/$randomId"
+            val scpCommand = "scp -r -i $keyPair $serverDirPath $sshTarget:$fullJarsDir/"
+            val relativeJarsDir = "./$allJarsDir"
+            val removeCommand = "sudo find $relativeJarsDir -type f -not -path \"$relativeJarsDir/$randomId/*\" -delete"
+            val cleanDirsCommand = "sudo find $fullJarsDir -empty -type d -delete"
+            val jarDir = "$fullJarsDir/$randomId"
             val logFile = "$jarDir/output.txt"
             val javaCommand = "nohup sudo java -jar $jarDir/$jarName >$logFile 2>$logFile <$logFile &"
 
@@ -145,18 +184,49 @@ tasks {
             val sshCommand2 = "$sshCommandPrefix \"$javaCommand\""
 
             doFirst {
-                println("Uploading server jar $serverJarPath to EC2 instance at $domain with id $randomId...")
-                // Create the server jar directory that will be transferred to the server
-                serverDir.toFile().mkdirs()
-                serverJar.copyTo(serverDir.resolve(serverJar.name).toFile())
+                if(keyPair == null) error("Environment variable EC2_KEYPAIR is not set!")
+                if(domain == null) error("Project variable ec2_domain is not set!")
 
-                println("Running '$scpCommand'")
-                runCommand(scpCommand)
-                println("Upload done, Running '$sshCommand1'")
-                runCommand( sshCommand1)
-                println("Cleanup done, Running '$sshCommand2'")
-                runCommand( sshCommand2)
-                println("Update successful.")
+//                val ssh = SshClient("")
+//                ScpClient()
+
+                runCommandToStd("testscript2.cmd")
+//                ByteArrayOutputStream().use { baos ->
+////                    runBlocking {
+////                        val job = launch {
+//                            exec {
+//                                commandLine = "scp -r -i C:\\Users\\natan\\Desktop\\GoogleDrivedSynced\\GoogleDriveBackup\\aws_secret\\AcEC2Pair.pem C:\\Users\\natan\\Desktop\\Crashy\\server\\build\\libs\\4872830908762331994 ec2-user@ec2-35-157-64-36.eu-central-1.compute.amazonaws.com:~/ac/".split(" ")
+//                                standardOutput = baos
+//                            }
+////                        }
+//
+//                    println ("Output:\n$baos")
+//
+////                        while(job.isActive){
+////                            println(baos.toString())
+////                            delay(500)
+////                        }
+////                    }
+//
+//                }
+//                println("done running command")
+
+//                runCommandToStd()
+
+//                println("Uploading server jar $serverJarPath to EC2 instance at $domain with id $randomId...")
+//                // Create the server jar directory that will be transferred to the server
+//                serverDir.toFile().mkdirs()
+//                serverJar.copyTo(serverDir.resolve(serverJar.name).toFile())
+//
+////                runCommandToStd("amar")
+//
+//                println("Running '$scpCommand'")
+//                runCommandToStd(scpCommand)
+//                println("Uploaded server to $fullJarsDir in the EC2 instance, Running '$sshCommand1'")
+//                runCommandToStd( sshCommand1)
+//                println("Cleanup done, Running '$sshCommand2'")
+//                runCommandToStd( sshCommand2)
+//                println("Update successful.")
             }
 
         }
@@ -167,16 +237,29 @@ tasks {
 fun String.trimNewlines() = replace("\n", "").replace("\r", "")
 
 fun runCommandToStd(command: String) {
-    val proc = ProcessBuilder(*command.split(" ").toTypedArray())
-        .inheritIO()
-        .redirectOutput(Redirect.PIPE)
-        .redirectError(Redirect.PIPE)
-        .start()
+    println(command)
+    val proc = Runtime.getRuntime().exec(command)
 
-    inheritIO(proc.inputStream,System.out)
-    inheritIO(proc.errorStream,System.err)
+    val inputStream: InputStream = proc.inputStream
+    val inputStreamReader = InputStreamReader(inputStream)
+    val bufferedReader = BufferedReader(inputStreamReader)
 
-    proc.waitFor(60, TimeUnit.MINUTES)
+    var line: String?
+    while (bufferedReader.readLine().also { line = it } != null) {
+        println(line) // it prints all at once after command has been executed.
+    }
+    proc.waitFor()
+
+//    val proc = ProcessBuilder(*command.split(" ").toTypedArray())
+//        .inheritIO()
+//        .redirectOutput(Redirect.PIPE)
+//        .redirectError(Redirect.PIPE)
+//        .start()
+
+//    inheritIO(proc.inputStream,System.out)
+//    inheritIO(proc.errorStream,System.err)
+//
+//    proc.waitFor(60, TimeUnit.MINUTES)
 }
 
 fun inheritIO(src: java.io.InputStream, dest: java.io.PrintStream) {
--- server/settings.gradle.kts
+++ server/settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id ("de.fayard.refreshVersions") version "0.50.2"
 }
-rootProject.name = "AntiCancerServer"
+rootProject.name = "Crashy"
 refreshVersions {
     rejectVersionIf {
         candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
