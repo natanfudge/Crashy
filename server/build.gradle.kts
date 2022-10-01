518e66bde27dd0da20d4dc0a70d7585f2745ac97
natanfudge <natan.lifsiz@gmail.com>
2022-10-01 09:52:26 +0300

    very nice! server is online!

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,25 +1,24 @@
+import com.sshtools.client.SessionChannelNG
 import com.sshtools.client.SshClient
 import com.sshtools.client.scp.ScpClient
-import java.util.*
+import com.sshtools.client.tasks.AbstractCommandTask
+import com.sshtools.client.tasks.FileTransferProgress
+import me.tongfei.progressbar.ProgressBarBuilder
+import java.nio.charset.Charset
 import kotlin.math.absoluteValue
 import kotlin.random.Random
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.InputStream
-import java.io.ByteArrayOutputStream
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import java.nio.file.*
+
 buildscript {
     repositories {
-        maven (
+        maven(
             url = ("https://oss.sonatype.org/content/repositories/snapshots")
         )
     }
 
     dependencies {
-        classpath ("com.sshtools:maverick-synergy-client:3.0.6")
+        classpath("com.sshtools:maverick-synergy-client:3.0.9")
+        classpath("me.tongfei:progressbar:0.9.4")
     }
 }
 plugins {
@@ -46,9 +45,9 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 repositories {
     mavenCentral()
-        maven (
-            url = ("https://oss.sonatype.org/content/repositories/snapshots")
-        )
+    maven(
+        url = ("https://oss.sonatype.org/content/repositories/snapshots")
+    )
 
 
 }
@@ -56,9 +55,8 @@ repositories {
 val invoker = configurations.create("invoker")
 
 dependencies {
-    implementation (libs.bundles.main)
+    implementation(libs.bundles.main)
     implementation(libs.bundles.test)
-    implementation("com.sshtools:maverick-synergy-client:3.0.6")
 }
 
 
@@ -105,7 +103,7 @@ tasks {
     // scp new file
     // start process
 
-    register("testServer"){
+    register("testServer") {
         doLast {
 //            runCommand("echo starting && ssh -i C:\\Users\\natan\\Desktop\\GoogleDriveBackup\\aws_secret\\AcEC2Pair.pem ec2-user@ec2-18-195-90-15.eu-central-1.compute.amazonaws.com \"nohup sudo java -jar ~/ac/2325446946910435587/AntiCancerServer-0.0.2-all.jar >~/ac/2325446946910435587/output.txt 2>~/ac/2325446946910435587/output.txt <~/ac/2325446946910435587/output.txt &\"")
             runCommand("echo starting && ssh -i C:\\Users\\natan\\Desktop\\GoogleDriveBackup\\aws_secret\\AcEC2Pair.pem ec2-user@ec2-18-195-90-15.eu-central-1.compute.amazonaws.com \"sudo killall java ; sudo find ./ac -type f -not -path \"./ac/5388493444252912057/*\" -delete && sudo find ~/ac -empty -type d -delete && nohup sudo java -jar ~/ac/5388493444252912057/AntiCancerServer-0.0.2-all.jar >~/ac/5388493444252912057/output.txt 2>~/ac/5388493444252912057/output.txt <~/ac/5388493444252912057/output.txt &\"")
@@ -122,7 +120,7 @@ tasks {
             dependsOn(shadowJar)
 
             workingDir(serverJar.parent)
-            commandLine("java", "-jar",serverJar.name)
+            commandLine("java", "-jar", serverJar.name)
         }
 
         /**
@@ -168,7 +166,7 @@ tasks {
             val allJarsDir = "jars"
             val fullJarsDir = "~/$allJarsDir"
 
-            val scpCommand = "scp -r -i $keyPair $serverDirPath $sshTarget:$fullJarsDir/"
+//            val scpCommand = "scp -r -i $keyPair $serverDirPath $sshTarget:$fullJarsDir/"
             val relativeJarsDir = "./$allJarsDir"
             val removeCommand = "sudo find $relativeJarsDir -type f -not -path \"$relativeJarsDir/$randomId/*\" -delete"
             val cleanDirsCommand = "sudo find $fullJarsDir -empty -type d -delete"
@@ -180,17 +178,27 @@ tasks {
             val remoteCleanupCommand = "$killCommand ; $removeCommand && $cleanDirsCommand"
 
             // Split cleanup command and java -jar command so the task will exit properly
-            val sshCommand1 = "$sshCommandPrefix \"$remoteCleanupCommand\""
-            val sshCommand2 = "$sshCommandPrefix \"$javaCommand\""
+//            val sshCommand1 = "$sshCommandPrefix \"$remoteCleanupCommand\""
+//            val sshCommand2 = "$sshCommandPrefix \"$javaCommand\""
 
             doFirst {
-                if(keyPair == null) error("Environment variable EC2_KEYPAIR is not set!")
-                if(domain == null) error("Project variable ec2_domain is not set!")
+                if (keyPair == null) error("Environment variable EC2_KEYPAIR is not set!")
+                if (domain == null) error("Project variable ec2_domain is not set!")
+
+                // Create the server jar directory that will be transferred to the server
+                serverDir.toFile().mkdirs()
+                serverJar.copyTo(serverDir.resolve(serverJar.name).toFile())
+
+                ssh(host = domain, username = "ec2-user", keyPair = File(keyPair)) {
+                    scp(from = serverDir, to = fullJarsDir)
+                    execute(remoteCleanupCommand)
+                    execute(javaCommand)
+                }
 
 //                val ssh = SshClient("")
 //                ScpClient()
 
-                runCommandToStd("testscript2.cmd")
+//                runCommandToStd("testscript2.cmd")
 //                ByteArrayOutputStream().use { baos ->
 ////                    runBlocking {
 ////                        val job = launch {
@@ -214,9 +222,7 @@ tasks {
 //                runCommandToStd()
 
 //                println("Uploading server jar $serverJarPath to EC2 instance at $domain with id $randomId...")
-//                // Create the server jar directory that will be transferred to the server
-//                serverDir.toFile().mkdirs()
-//                serverJar.copyTo(serverDir.resolve(serverJar.name).toFile())
+
 //
 ////                runCommandToStd("amar")
 //
@@ -236,40 +242,40 @@ tasks {
 
 fun String.trimNewlines() = replace("\n", "").replace("\r", "")
 
-fun runCommandToStd(command: String) {
-    println(command)
-    val proc = Runtime.getRuntime().exec(command)
-
-    val inputStream: InputStream = proc.inputStream
-    val inputStreamReader = InputStreamReader(inputStream)
-    val bufferedReader = BufferedReader(inputStreamReader)
-
-    var line: String?
-    while (bufferedReader.readLine().also { line = it } != null) {
-        println(line) // it prints all at once after command has been executed.
-    }
-    proc.waitFor()
-
-//    val proc = ProcessBuilder(*command.split(" ").toTypedArray())
-//        .inheritIO()
-//        .redirectOutput(Redirect.PIPE)
-//        .redirectError(Redirect.PIPE)
-//        .start()
-
-//    inheritIO(proc.inputStream,System.out)
-//    inheritIO(proc.errorStream,System.err)
+//fun runCommandToStd(command: String) {
+//    println(command)
+//    val proc = Runtime.getRuntime().exec(command)
 //
-//    proc.waitFor(60, TimeUnit.MINUTES)
-}
-
-fun inheritIO(src: java.io.InputStream, dest: java.io.PrintStream) {
-    Thread {
-        val sc = Scanner(src)
-        while (sc.hasNextLine()) {
-            dest.println(sc.nextLine())
-        }
-    }.start()
-}
+//    val inputStream: InputStream = proc.inputStream
+//    val inputStreamReader = InputStreamReader(inputStream)
+//    val bufferedReader = BufferedReader(inputStreamReader)
+//
+//    var line: String?
+//    while (bufferedReader.readLine().also { line = it } != null) {
+//        println(line) // it prints all at once after command has been executed.
+//    }
+//    proc.waitFor()
+//
+////    val proc = ProcessBuilder(*command.split(" ").toTypedArray())
+////        .inheritIO()
+////        .redirectOutput(Redirect.PIPE)
+////        .redirectError(Redirect.PIPE)
+////        .start()
+//
+////    inheritIO(proc.inputStream,System.out)
+////    inheritIO(proc.errorStream,System.err)
+////
+////    proc.waitFor(60, TimeUnit.MINUTES)
+//}
+//
+//fun inheritIO(src: java.io.InputStream, dest: java.io.PrintStream) {
+//    Thread {
+//        val sc = Scanner(src)
+//        while (sc.hasNextLine()) {
+//            dest.println(sc.nextLine())
+//        }
+//    }.start()
+//}
 
 fun runCommand(command: String): String {
     val parts = command.split("\\s".toRegex())
@@ -281,3 +287,54 @@ fun runCommand(command: String): String {
     return proc.inputStream.bufferedReader().readText()
 }
 
+
+fun ssh(host: String, username: String, keyPair: File, session: SSHSession.() -> Unit) {
+    println("SSHing to $host...")
+    SshClient(host, 22, username, keyPair).use {
+        println("SSH successful")
+        SSHSession(it).apply(session)
+    }
+}
+
+class SSHSession(private val ssh: SshClient) {
+    fun scp(from: java.nio.file.Path, to: String) {
+        println("Uploading ${from.toAbsolutePath()} to $to")
+        ProgressBarBuilder().setTaskName("Upload").setUpdateIntervalMillis(200).build().use { progressBar ->
+            ScpClient(ssh).put(from.toAbsolutePath().toString(), to, true, object : FileTransferProgress {
+                override fun started(bytesTotal: Long, remoteFile: String?) {
+                    progressBar.maxHint(bytesTotal)
+                }
+
+                override fun isCancelled(): Boolean {
+                    return false
+                }
+
+                override fun progressed(bytesSoFar: Long) {
+                    progressBar.stepTo(bytesSoFar)
+                }
+
+                override fun completed() {
+                }
+            })
+        }
+        println("Upload successful")
+    }
+
+    fun execute(command: String) {
+        println("Executing $command")
+        val task: AbstractCommandTask = object : AbstractCommandTask(ssh.connection, command) {
+            override fun onOpenSession(session: SessionChannelNG) {
+                val stdOutput = session.inputStream.readAllBytes().toString(Charset.defaultCharset())
+                val errOutput = session.stderrStream.readAllBytes().toString(Charset.defaultCharset())
+                if (stdOutput.isNotBlank()) {
+                    println("SSH: $stdOutput")
+                }
+                if (errOutput.isNotBlank()) {
+                    println("SSH ERR: $errOutput")
+                }
+            }
+        }
+
+        ssh.runTask(task)
+    }
+}
