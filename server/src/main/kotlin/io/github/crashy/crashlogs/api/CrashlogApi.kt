766d66bc27c3f2a5b07dbb7459859732e2480dcb
natanfudge <natan.lifsiz@gmail.com>
2023-02-19 21:32:03 +0200

    increase max crash size

--- server/src/main/kotlin/io/github/crashy/crashlogs/api/CrashlogApi.kt
+++ server/src/main/kotlin/io/github/crashy/crashlogs/api/CrashlogApi.kt
@@ -14,7 +14,7 @@ import java.time.Instant
 import kotlin.io.path.readText
 
 
-private const val MaxCrashSize = 100_000
+private const val MaxCrashSize = 200_000
 
 
 class CrashlogApi(private val logs: CrashlogStorage) {
