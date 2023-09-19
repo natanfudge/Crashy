1add005fcf15019da43479d4a019506a843c7e11
natanfudge <natan.lifsiz@gmail.com>
2023-09-19 08:48:11 +0300

    should fix blacklisting

--- server/src/main/kotlin/io/github/crashy/crashlogs/Blacklist.kt
+++ server/src/main/kotlin/io/github/crashy/crashlogs/Blacklist.kt
@@ -3,7 +3,7 @@ package io.github.crashy.crashlogs
 import io.github.crashy.utils.getResource
 
 object Blacklist {
-    private val items = getResource("/secrets/blacklist.txt")!!.split("\n").toHashSet()
+    private val items = getResource("/secrets/blacklist.txt")!!.split("\n").map { it.trim() }.toHashSet()
     fun isBlacklisted(ip: String): Boolean {
         return ip in items
     }
