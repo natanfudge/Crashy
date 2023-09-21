92fd8463e4ccc274a76690da38cc8e026ea341c5
natanfudge <natan.lifsiz@gmail.com>
2023-09-21 17:16:40 +0300

    move to submodules

--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-git commit -am "$1"
+git add .
+git commit -m "$1"
 git push
 #It will always push to main which is not optimal but works for now
+git submodule foreach git add .
 git submodule foreach git commit -am "$1"
 git submodule foreach git push origin HEAD:main
